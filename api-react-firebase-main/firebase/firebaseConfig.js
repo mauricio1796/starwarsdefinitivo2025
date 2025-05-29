142499862b8c22eb2dc581d564b92f579ccc79c1
mauricio1796 <ricardo.mosquerab@uniagustiniana.edu.co>
2025-05-29 00:03:42 +0000

    firebase

--- api-react-firebase-main/firebase/firebaseConfig.js
+++ api-react-firebase-main/firebase/firebaseConfig.js
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFKPaT4qKq9RIVh_qva3WASVY8mxJ8ZnQ",
-  authDomain: "pokeapi2-ea696.firebaseapp.com",
-  projectId: "pokeapi2-ea696",
-  storageBucket: "pokeapi2-ea696.firebasestorage.app",
-  messagingSenderId: "47412692126",
-  appId: "1:47412692126:web:c6b21b67d9a74d4da1a78f"
+  apiKey: "AIzaSyDmxw5CSTJMn0JZE-dtU2uF6ok4Rt-fwiQ",
+  authDomain: "starwarsapi1.firebaseapp.com",
+  projectId: "starwarsapi1",
+  storageBucket: "starwarsapi1.firebasestorage.app",
+  messagingSenderId: "124140579449",
+  appId: "1:124140579449:web:40d6598680727ec5ab02a8"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app); // ✅ ¡Esto es necesario!
